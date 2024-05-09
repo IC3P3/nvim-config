5083f0de89d10b8f80efd0ec97ed296dcf404b12
Niklas Eifler <neifler@niklas-home-pc.fritz.box>
2024-05-09 21:43:15 +0200

    Changed default settings for nvim-presence

--- lua/custom/plugins/presence.lua
+++ lua/custom/plugins/presence.lua
@@ -1,11 +1,24 @@
 return {
-  -- Discord rich presence integration for NeoVim
+  -- Discord rich presence integration for Neovim
   'andweeb/presence.nvim',
 
-  --[[config = function()
-    local presence = require 'presence.nvim'
-
-    presence.setup {}
-  end,]]
-  --
+  config = function()
+    require('presence').setup {
+      auto_update = true,
+      neovim_image_text = 'I use Neovim btw!',
+      main_image = 'file',
+      enable_line_number = true,
+      buttons = {
+        {
+          label = 'Forgejo',
+          url = 'https://git.eiflerstrom.de/IC3P3',
+        },
+        {
+          label = 'GitHub',
+          url = 'https://github.com/IC3P3',
+        },
+      },
+      show_timer = true,
+    }
+  end,
 }
