95a6be05038cf86b63166546bd140b6c8f459c05
Niklas Eifler <neifler@niklas-home-pc.fritz.box>
2024-05-09 21:50:34 +0200

    Added comments to precense.nvim

--- lua/custom/plugins/presence.lua
+++ lua/custom/plugins/presence.lua
@@ -2,12 +2,19 @@ return {
   -- Discord rich presence integration for Neovim
   'andweeb/presence.nvim',
 
+  -- Function to set default setting and change default settings
   config = function()
+    -- Call the setup presence.nvim and change settings
     require('presence').setup {
+      -- Automatically update the activity when changing file, interface or whatever
       auto_update = true,
+      -- Text displayed when hovering over the image
       neovim_image_text = 'I use Neovim btw!',
+      -- The image is changed to the type of file opened (with 'neovim' it is always an Neovim image)
       main_image = 'file',
+      -- Show the line you are currently in (somewhat infrequently)
       enable_line_number = true,
+      -- Add buttons to beneath the activity and can open an url
       buttons = {
         {
           label = 'Forgejo',
@@ -18,6 +25,7 @@ return {
           url = 'https://github.com/IC3P3',
         },
       },
+      -- Enables the times how long you are in Neovim
       show_timer = true,
     }
   end,
