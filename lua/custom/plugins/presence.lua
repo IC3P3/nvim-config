219dd6416863e7dc583ef4c125dd0d85e49eb0ba
Niklas Eifler <neifler@niklas-home-pc.fritz.box>
2024-05-09 21:12:19 +0200

    Added discord rpc integration

---
+++ lua/custom/plugins/presence.lua
@@ -0,0 +1,11 @@
+return {
+  -- Discord rich presence integration for NeoVim
+  'andweeb/presence.nvim',
+
+  --[[config = function()
+    local presence = require 'presence.nvim'
+
+    presence.setup {}
+  end,]]
+  --
+}
