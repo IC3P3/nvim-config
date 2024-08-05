2bf448a3e665f8db8cd9607872e1e2ca0c600691
IC3P3 <n.eifler@freenet.de>
2024-08-05 11:34:52 +0200

    Added my needed Mason and Treesitter installations

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -165,16 +165,8 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        rust_analyzer = {},
+        tsserver = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
