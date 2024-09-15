505c61045d67f73b074977069db5a889de264959
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-09-15 13:57:08 +0200

    Added linting to js and ts

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -189,11 +189,15 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+<<<<<<< HEAD
         'eslint_d',
         'prettier',
         'prettierd',
         'nixpkgs-fmt',
         'markdownlint',
+=======
+        'eslint',
+>>>>>>> 5552a30 (Added linting to js and ts)
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
