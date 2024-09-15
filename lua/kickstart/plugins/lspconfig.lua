bbe4fa8f8fc34402a25ccb83c0e31770ff589e75
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-09-15 14:22:12 +0200

    Added autoformatting to js and ts

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -168,6 +168,8 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'eslint',
+        'prettier',
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
