87f8b4a63df18a26a8b03897369847796e72996d
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-09-15 17:30:58 +0200

    Removed go debugger

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -192,6 +192,7 @@ return {
         'eslint',
         'prettier',
         'prettierd',
+        'js-debug-adapter',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
