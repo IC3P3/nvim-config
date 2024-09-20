bdb101e474dd8de9a6698b9ea701e0d0fe959f7f
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-09-20 12:08:58 +0200

    Added svelte and tailwind lsp to mason

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -186,7 +186,7 @@ return {
         'nixpkgs-fmt',
         'markdownlint',
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('m<script lang="ts">ason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
         handlers = {
