a047aba6e2921481cba5cfaee0040ac6ea69dd72
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-09-20 12:48:04 +0200

    Fixed lspconfig

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -194,7 +194,7 @@ return {
         'eslint',
         'jq',
       })
-      require('m<script lang="ts">ason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
         handlers = {
