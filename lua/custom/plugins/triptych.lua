2cb8c0242e53687617954684539bb555d341494e
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-06 09:00:33 +0100

    Added telescope searching to triptych

--- lua/custom/plugins/triptych.lua
+++ lua/custom/plugins/triptych.lua
@@ -7,7 +7,18 @@ return {
       'nvim-tree/nvim-web-devicons',
       'antosha417/nvim-lsp-file-operations',
     },
-    opts = {},
+    opts = {
+      extension_mappings = {
+        ['<c-f>'] = {
+          mode = 'n',
+          fn = function(target, _)
+            require('telescope.builtin').live_grep {
+              search_dirs = { target.path },
+            }
+          end,
+        },
+      },
+    },
     keys = {
       { '<leader>-', ':Triptych<CR>' },
     },
