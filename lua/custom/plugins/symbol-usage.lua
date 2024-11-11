95df425bce202fc83522feeb7718860353ea9954
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-11 12:40:10 +0100

    Fixed symbol usage warning

--- lua/custom/plugins/symbol-usage.lua
+++ lua/custom/plugins/symbol-usage.lua
@@ -3,7 +3,23 @@ return {
     'Wansmer/symbol-usage.nvim',
     event = 'LspAttach',
     config = function()
-      require('symbol-usage').setup()
+      require('symbol-usage').setup {
+        log = {
+          enabled = false,
+          level = 'ERROR',
+          stdout = {
+            enabled = true,
+          },
+          log_file = {
+            enabled = false,
+            path = '',
+          },
+          notify = {
+            enabld = true,
+          },
+        },
+        filetypes = {},
+      }
     end,
   },
 }
