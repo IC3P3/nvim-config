34e176705d5e8f3461178422762b26ea284a72ca
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-26 14:48:34 +0100

    Added plain noice.nvim

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+    -- TODO: No configuration added, yet
+  },
+}
